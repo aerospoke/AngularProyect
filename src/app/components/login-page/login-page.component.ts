3a07f5ca8c1136d94205d7c9745d454cbb9b85ce
Jonathan <jonathanbf2000@gmail.com>
2025-02-07 18:37:34 -0500

    Forms working with data

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
-  imports: [IonicModule],
+  imports: [IonicModule,FormsModule],
   
 })
 export class LoginPageComponent  implements OnInit {
 
+  public dataLogin = {
+    username:'',
+    password:''
+  }
   constructor(private router: Router) { }
 
   ngOnInit() {}
 
   clickButtonBack(){    
     this.router.navigate(['/home']);
+  }
+  submitButton(){
+    console.log("llego esta info para iniciar sesion");
+    console.log(this.dataLogin);
+    
   }
 
 }
--- src/app/components/register-page/register-page.component.ts
+++ src/app/components/register-page/register-page.component.ts
@@ -1,20 +1,34 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [IonicModule,], 
+  imports: [IonicModule,FormsModule], 
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.scss'],
 })
 export class RegisterPageComponent {
 
+  public userData = {
+    name:'',
+    password:'',
+    gender:''
+  }
+
   constructor(private router: Router) {}
 
   clickButtonBack(){    
     this.router.navigate(['/home']);
+  }
+
+  submitButton(){
+    console.log("llego esta info para empezar el registro");
+    console.log(this.userData);
+    
   }
 }
