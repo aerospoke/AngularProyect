1d5b48ce4134ee31c7dd7378ea1c1979f6f386af
Jonathan <jonathanbf2000@gmail.com>
2025-02-09 15:39:26 -0500

    building profile-page

--- src/app/components/profile-page/profile-page.component.ts
+++ src/app/components/profile-page/profile-page.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss'],
+  imports:[IonicModule,CommonModule,FormsModule]
 })
 export class ProfilePageComponent  implements OnInit {
 
-  constructor() { }
+  public userData = {
+    name:'',
+    email:'',
+    password:'',
+    country:'',
+    selectedColor:'',
+  }
+  constructor(private router:Router) { }
+  
 
   ngOnInit() {}
+
+  clickButtonBack(){    
+    this.router.navigate(['/home']);
+  }
 
 }
